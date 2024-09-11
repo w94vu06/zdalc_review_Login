2e51786a683553dbfe392b0c77638a8fa5805d05
doob925 <doob925@gmail.com>
2024-09-11 17:03:54 +0800

    feat: Add configuration files and types
    - Added .eslintrc.json for ESLint configuration
    - Added prettier.config.js for Prettier configuration
    - Added postcss.config.js for PostCSS configuration
    - Added next.config.mjs for Next.js configuration
    - Added jsvectormap.d.ts for TypeScript declaration
    - Added .idea/.gitignore to ignore default files in IntelliJ IDEA
    - Added .idea/vcs.xml for VCS directory mappings
    - Added .idea/prettier.xml for Prettier configuration in IntelliJ IDEA
    - Added .idea/modules.xml for project module management
    - Added .gitignore to ignore dependencies, build files, and local environment files
    - Added src/types/faq.ts for FAQ type definition
    - Added src/types/country.ts for Country type definition
    - Added src/types/package.ts for Package type definition
    - Added src/types/chat.ts for Chat type definition
    - Added src/types/Lead.ts for Lead type definition
    - Added src/types/brand.ts for BRAND type definition
    - Added src/types/product.ts for Product type definition
    - Added src/types/faqItem.ts for FaqItem type definition
    - Added src/types/cards.ts for CardItemProps type definition
    - Added src/components/common/Loader/index.tsx for Loader component
    - Added src/app/page.tsx for Home page component
    - Added src/app/tables/page.tsx for Tables page component
    - Added src/app/chart/page.tsx for Chart page component
    - Added src/hooks/useColorMode.tsx for custom hook to manage color mode
    - Added public/images/icon/icon-arrow-down.svg for arrow down icon
    - Added public/images/brand/brand-05.svg for brand logo
    - Added src/components/Breadcrumbs/Breadcrumb.tsx for Breadcrumb component
    - Updated tsconfig.json to include new files and paths
    - Updated package.json with new dependencies and devDependencies

---
+++ src/app/tables/page.tsx
@@ -0,0 +1,18 @@
+import Breadcrumb from "@/components/Breadcrumbs/Breadcrumb";
+import TableOne from "@/components/Tables/TableOne";
+
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+const TablesPage = () => {
+  return (
+    <DefaultLayout>
+      <Breadcrumb pageName="文章審查" />
+
+      <div className="flex flex-col gap-10">
+        <TableOne />
+      </div>
+    </DefaultLayout>
+  );
+};
+
+export default TablesPage;
